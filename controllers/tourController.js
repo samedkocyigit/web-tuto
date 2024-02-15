52ef9ccbe7c9b98da9eb2dff7645f2fab52158d2
abdulsamed koçyiğit <abdulsamed.kcygt@gmail.com>
2024-02-15 18:03:38 +0300

    matching and grouping has been defined with used aggregation pipeline

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -105,6 +105,46 @@ exports.deleteTour = async (req, res) => {
     res.status(404).json({
       status:'fail',
       message:'yok oyle bisi'
-    })
+    });
   }
 };
+
+exports.getTourStats = async (req,res)=>{
+  try{
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: {$gte: 4.5}},
+      },
+      {
+        $group:{
+          _id: null,
+          numTours: { $sum: 1 },
+          numRatings: { $sum: 'ratingQuantity'},
+          avgRating: { $avg: '$ratingAverage'},
+          avgPrice: { $avg: '$price'},
+          minPrice: { $min: '$price'},
+          maxPrice: { $max: '$price'}
+        }
+      },
+      {
+        $sort:{ avgPrice: 1 }
+      }
+      // {
+      //   $match: { _id: {$ne: 'EASY' } }
+      // }
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data:{
+        stats
+      }
+    });
+  }
+  catch(err){
+    res.status(404).json({
+      status:'fail',
+      message:err
+    })
+  }
+}
