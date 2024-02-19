9c214644bf1689322f1b4a6bcafad2fc8c58b91b
abdulsamed koçyiğit <abdulsamed.kcygt@gmail.com>
2024-02-19 18:17:45 +0300

    Some user routes and controller has been created and defined

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,30 +1,56 @@
+const catchAsync = require("../utils/catchAsync");
+const User = require('../models/userModel');
+const AppError = require("../utils/appError");
+
 exports.getAllUsers = (req, res) => {
   res.status(500).json({
     status: 'error',
     message: 'This route is not yet defined!'
   });
 };
-exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!'
+
+exports.getUser = catchAsync(async (req, res,next) => {
+  const user = User.findById(req.params.id)
+  
+  if(!user){
+    return next( new AppError('Invalid ID',404))
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user
+    }
   });
-};
+});
+
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
     message: 'This route is not yet defined!'
   });
 };
-exports.updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!'
+exports.updateUser = catchAsync(async (req, res) => {
+  const user = User.findByIdAndUpdate(req.body)
+
+  if(!user){
+    return new AppError('Invalid ID',404)
+  }
+
+  res.status(200).json({
+    status: 'success',
+    message: 'This user is updated succesfuly'
   });
-};
-exports.deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!'
+});
+exports.deleteUser =catchAsync(async (req, res) => {
+  const user = await User.findByIdAndRemove(req.params.id)
+
+  if(!user){
+    return new AppError('Invalid ID',404)
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data:null
   });
-};
+});
