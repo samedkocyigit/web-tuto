11a72a35229a5061817d9a8e80fde6d871fc2d9e
abdulsamed koçyiğit <abdulsamed.kcygt@gmail.com>
2024-02-20 15:17:08 +0300

    passwords has been encrypted with bcryptejs.

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 
+const brcrypt = require('bcryptjs')
+
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -20,17 +22,45 @@ const userSchema = new mongoose.Schema({
     password:{
         type:String,
         required: [true,'please provide a password'],
-        minlength:8
+        minlength:8,
+        select: false
     },
     passwordConfirm:{
         type:String,
         required: [true,'please provide a password'],
-        validator: function(el){
+        validate: {
+          validator: function(el){
             return el === this.password
+          },
+          message:'Password are not the same!!!!'
         }
-    }
+    },
+    passwordChangedAt: Date
 })
 
+userSchema.pre('save',async function(next){
+    // Only run this function if password was actually modified
+    if(!this.isModified('password')) return next() 
+
+    this.password= await brcrypt.hash(this.password,12)
+
+    this.passwordConfirm=undefined
+    next()
+})
+
+userSchema.methods.correctPassword= async function(candidatePassword,userPassword){
+    return await brcrypt.compare(candidatePassword,userPassword)
+}
+
+userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
+    if(this.passwordChangedAt){
+        const changedTimestamp = parseInt(this.passwordChangedAt.getTime()/1000,10)
+        
+        return JWTTimestamp<changedTimestamp
+    }
+
+    return false;
+}
 const User = mongoose.model('User',userSchema)
 
 module.exports = User
