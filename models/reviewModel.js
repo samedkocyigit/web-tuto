01947c7bcf97e59cf4707ff5a3fe4258ae7ca5cf
abdulsamed koçyiğit <abdulsamed.kcygt@gmail.com>
2024-02-22 18:13:26 +0300

    small updates

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -30,14 +30,19 @@ const reviewSchema = new mongoose.Schema(
 // QUERY MIDDLEWARE
 
 reviewSchema.pre(/^find/, function(next){
+  // this.populate({
+  //   path: 'tour',
+  //   select: 'name'
+  // }).populate({
+  //   path: 'user',
+  //   select: 'name'
+  // });
+
   this.populate({
-    path: 'tour',
-    select: 'name'
-  }).populate({
     path: 'user',
-    select: 'name photo'
-  });
-  
+    select: 'name'
+  })
+
   next()
 })
 const Review = mongoose.model("Review",reviewSchema)
