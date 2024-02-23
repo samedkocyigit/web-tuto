9dff4974ebac5ad0ec1aa0748ee3f022754993bd
abdulsamed koçyiğit <abdulsamed.kcygt@gmail.com>
2024-02-23 18:04:54 +0300

    codes refactored for clean sheet with factory func

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,7 +1,6 @@
-const AppError = require('../utils/appError');
+//const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const Tour = require('./../models/tourModel');
-const APIFeatures = require('./../utils/apiFeatures')
 const factory= require("./handlerFactory")
 
 
@@ -12,40 +11,8 @@ exports.aliasTopTours=(req,res,next) => {
   next();
 };
 
-exports.getAllTours =catchAsync(async (req, res,next) => {
-  //Execute Query
-  const features = new APIFeatures(Tour.find(),req.query)
-  .filter()
-  .sort()
-  .limitFields()
-  .paginate();
-  const tours= await features.query;
-
-  //const tours= await Tour.find( )
-
-  res.status(200).json({
-    status: 'success',
-    requestedAt: tours.length,
-    data:{
-      tours  
-    }
-  });
-});
-
-exports.getTour =catchAsync( async(req, res,next) => {
-  const tour = await Tour.findById(req.params.id).populate('reviews')
-
-  if(!tour){
-    return next(new AppError('No tour found with that ID',404))
-  }
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: tour
-    }
-  });
-});
-
+exports.getAllTours = factory.getAll(Tour)
+exports.getTour = factory.getOne(Tour, {path: "reviews"})
 exports.createTour = factory.createOne(Tour)
 exports.updateTour = factory.updateOne(Tour)
 exports.deleteTour = factory.deleteOne(Tour)
