67fd1b454a646846e83d69a08e3fcaebd5d9f74a
abdulsamed koçyiğit <abdulsamed.kcygt@gmail.com>
2024-02-23 12:26:37 +0300

    codes has been refactored for factory func

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,5 +1,7 @@
 const Review = require("../models/reviewModel");
 const catchAsync = require("../utils/catchAsync");
+const factory= require("./handlerFactory")
+
 
 exports.getAllReviews= catchAsync(async (req,res,next)=>{
   let filter={}
@@ -15,17 +17,13 @@ exports.getAllReviews= catchAsync(async (req,res,next)=>{
   })
 })
 
-exports.createReview= catchAsync(async (req,res,next)=>{
+exports.setTourUserIds = (req,res,next) =>{
   //Allow nested routes
   if(!req.body.tour) req.body.tour = req.params.tourId
   if(!req.body.user) req.body.user = req.user.id
+  next()
+}
 
-  const newReview =await Review.create(req.body)
- 
-  res.status(200).json({
-    status:200,
-    data:{
-      newReview
-    }
-  })
-})
+exports.createReview = factory.createOne(Review)
+exports.updateReview = factory.updateOne(Review)
+exports.deleteReview = factory.deleteOne(Review)
