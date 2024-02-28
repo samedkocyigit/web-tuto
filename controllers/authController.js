73d378dd839783be4716559ff962e136eff5446f
abdulsamed koçyiğit <abdulsamed.kcygt@gmail.com>
2024-02-28 10:14:23 +0300

    updates and refactoring

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,7 +17,7 @@ const createSendToken = (user, statusCode, res)=>{
   const token = signToken(user._id)
   const cookieOptions={
     expires: new Date(
-        Date.now()+ process.env.JWT_COOKIE_EXPIRES_IN*24*60*60*1000
+        Date.now() + process.env.JWT_COOKIE_EXPIRES_IN*24*60*60*1000
     ),
     httpOnly: true
   }
@@ -74,12 +74,22 @@ exports.login = catchAsync(async(req,res,next) => {
   })
 })
 
+exports.logout = (req, res) => {
+  res.cookie('jwt', 'loggedout', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true
+  });
+  res.status(200).json({ status: 'success' });
+};
+
 exports.protect = catchAsync(async(req,res,next)=>{
   // 1) Getting token and check of its there
   let token
 
   if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
     token = req.headers.authorization.split(' ')[1]
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
   }
   // console.log(token)
   if(!token){
@@ -107,8 +117,39 @@ exports.protect = catchAsync(async(req,res,next)=>{
 
   // Grant access to protected route
   req.user=currentUser
+  res.locals.user = currentUser;
   next()
 })
+// Only for rendered pages, no errors!
+exports.isLoggedIn = async (req, res, next) => {
+  if (req.cookies.jwt) {
+    try {
+      // 1) verify token
+      const decoded = await promisify(jwt.verify)(
+        req.cookies.jwt,
+        process.env.JWT_SECRET
+      );
+
+      // 2) Check if user still exists
+      const currentUser = await User.findById(decoded.id);
+      if (!currentUser) {
+        return next();
+      }
+
+      // 3) Check if user changed password after the token was issued
+      if (currentUser.changedPasswordAfter(decoded.iat)) {
+        return next();
+      }
+
+      // THERE IS A LOGGED IN USER
+      res.locals.user = currentUser;
+      return next();
+    } catch (err) {
+      return next();
+    }
+  }
+  next();
+};
 
 exports.restrictTo = (...roles) => {
   return (req,res,next) =>{
--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -75,7 +75,6 @@ exports.getOne = (Model,popOptions) => catchAsync(async (req, res,next) => {
   let query = Model.findById(req.params.id)
   if(popOptions) query = query.populate(popOptions)
   const doc = await query
-
   if (!doc) {
     return next(new AppError('No document found with that ID', 404));
   }
