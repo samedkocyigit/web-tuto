a47877cdfe5b002a46081652bc4dad6d60c80214
abdulsamed koçyiğit <abdulsamed.kcygt@gmail.com>
2024-02-29 13:35:27 +0300

    updated

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-const User = require('./userModel');
+// const User = require('./userModel');
 // const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
@@ -139,21 +139,21 @@ tourSchema.pre('save', function(next) {
   next();
 });
 
-tourSchema.pre('save', async function(next) {
-  const guidesPromises = this.guides.map(async id => await User.findById(id));
-  this.guides = await Promise.all(guidesPromises);
-  next();
-});
+// tourSchema.pre('save', async function(next) {
+//   const guidesPromises = this.guides.map(async id => await User.findById(id));
+//   this.guides = await Promise.all(guidesPromises);
+//   next();
+// });
 
-tourSchema.pre('save', function(next) {
-  console.log('Will save document...');
-  next();
-});
+// tourSchema.pre('save', function(next) {
+//   console.log('Will save document...');
+//   next();
+// });
 
-tourSchema.post('save', function(doc, next) {
-  console.log(doc);
-  next();
-});
+// tourSchema.post('save', function(doc, next) {
+//   console.log(doc);
+//   next();
+// });
 
 // QUERY MIDDLEWARE
 // tourSchema.pre('find', function(next) {
